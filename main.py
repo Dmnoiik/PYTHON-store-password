6e8e894a3cd9cb83c69b415cf768c3d41bbca09a
Dmnoiik <75478242+Dmnoiik@users.noreply.github.com>
2023-05-08 10:15:08 +0200

    Add files via upload
    Finished the password generator

--- main.py
+++ main.py
@@ -2,18 +2,22 @@ from tkinter import *
 from tkinter import messagebox
 from random import choice, randint, shuffle
 import pyperclip
+import json
+
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 def generate_password():
-    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+               'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
+               'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
     numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
     symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
-    letters_list = [choice(letters) for _ in range(randint(8,10))]
+    letters_list = [choice(letters) for _ in range(randint(8, 10))]
 
-    symbols_list = [choice(symbols) for _ in range(randint(2,4))]
+    symbols_list = [choice(symbols) for _ in range(randint(2, 4))]
 
-    numbers_list = [choice(numbers) for _ in range(randint(2,4))]
+    numbers_list = [choice(numbers) for _ in range(randint(2, 4))]
     password_list = letters_list + symbols_list + numbers_list
     shuffle(password_list)
 
@@ -28,18 +32,57 @@ def save_credentials():
     website = website_entry.get()
     login = login_entry.get()
     password = password_entry.get()
+    new_data = {website: {
+        "email": login,
+        "password": password
+    }}
 
     if len(website) == 0 or len(password) == 0:
         messagebox.showwarning(title='Error', message='One of the fields is empty, either password or website')
     else:
-        is_ok = messagebox.askokcancel(title=website,
-                                       message=f"These are the details entered: \nEmail: {login}\nPassword: {password}"
-                                               f" \nIs it okay to save?")
-        if is_ok:
-            with open('data.txt', mode='a') as file:
-                file.write(f"Page: {website} | login: {login} | Password: {password}\n")
-                website_entry.delete(0, END)
-                password_entry.delete(0, END)
+        try:
+            with open('data.json', mode='r') as file:
+                try:
+                    # data is a dictionary
+                    data = json.load(file)
+                except json.decoder.JSONDecodeError:
+                    # Empty dicitonary if nothing in the file
+                    data = {}
+        except FileNotFoundError:
+            data = new_data
+        else:
+            ## Update DICTIONARY -> adds new key if missing or updates
+            if website in data:
+                is_ok = messagebox.askokcancel(message='Password is already stored\nDo you want to overwrite it?')
+                if is_ok:
+                    data.update(new_data)
+
+            data.update(new_data)
+
+
+        finally:
+            with open('data.json', mode='w') as file:
+                json.dump(data, file, indent=4)
+            website_entry.delete(0, END)
+            password_entry.delete(0, END)
+
+
+def search():
+    website = website_entry.get()
+    try:
+        with open('data.json', 'r') as file:
+            data = json.load(file)
+    except FileNotFoundError:
+        messagebox.showwarning('Warning', message='No passwords are stored yet. Please store new password')
+
+    else:
+        try:
+            email = data[website]['email']
+            password = data[website]['password']
+            messagebox.showinfo(website, message=f"Email: {email}\nPassword: {password}")
+        except KeyError:
+            messagebox.showwarning(title='Empty',
+                                   message='Such website is not stored anywhere (or is empty)')
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -57,7 +100,7 @@ login_label = Label(text='Email/Username:', font=("Ariel", 10, 'normal'))
 password_label = Label(text='Password:', font=("Ariel", 10, 'normal'))
 
 # Entries
-website_entry = Entry(width=52)
+website_entry = Entry(width=42)
 website_entry.focus()
 login_entry = Entry(width=52)
 login_entry.insert(index=0, string='d.marszalkiewicz2000@gmail.com')
@@ -65,7 +108,8 @@ password_entry = Entry(width=30)
 
 # Buttons
 generate_button = Button(text='Generate Password', command=generate_password)
-add_button = Button(text='Add', width=44, command=save_credentials)
+add_button = Button(text='Add', width=35, command=save_credentials)
+search_button = Button(text="Search", command=search)
 
 # Grid
 canvas.grid(column=1, row=0)
@@ -73,11 +117,11 @@ website_label.grid(column=0, row=1)
 login_label.grid(column=0, row=2)
 password_label.grid(column=0, row=3)
 
-website_entry.grid(column=1, row=1, columnspan=2, sticky='w')
-login_entry.grid(column=1, row=2, columnspan=2, sticky='w')
-password_entry.grid(column=1, row=3, sticky='w')
+website_entry.grid(column=1, row=1, columnspan=2, sticky='EW')
+login_entry.grid(column=1, row=2, columnspan=2, sticky='EW')
+password_entry.grid(column=1, row=3, sticky='EW')
 
-generate_button.grid(column=2, row=3)
-add_button.grid(column=1, row=4, columnspan=2)
-
+generate_button.grid(column=2, row=3, sticky='EW')
+add_button.grid(column=1, row=4, columnspan=2, sticky='EW')
+search_button.grid(column=2, row=1, sticky='EW')
 window.mainloop()
